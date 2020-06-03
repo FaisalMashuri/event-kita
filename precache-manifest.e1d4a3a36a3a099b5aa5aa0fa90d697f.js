84b40e689238d5e18fb767853e08b04233b0c158
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2020-06-04 00:46:00 +0700

    Updates

---
+++ precache-manifest.e1d4a3a36a3a099b5aa5aa0fa90d697f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62c937f3cb6ade9776ad82ab16cbf0e",
+    "url": "/event-kita/index.html"
+  },
+  {
+    "revision": "b366b1054b4fcd05597b",
+    "url": "/event-kita/static/css/main.2de5607b.chunk.css"
+  },
+  {
+    "revision": "a6b7f22c3813ac90499d",
+    "url": "/event-kita/static/js/2.58b4df4b.chunk.js"
+  },
+  {
+    "revision": "023adeeefc855cc7e9413510ed02dab2",
+    "url": "/event-kita/static/js/2.58b4df4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b366b1054b4fcd05597b",
+    "url": "/event-kita/static/js/main.a079c71a.chunk.js"
+  },
+  {
+    "revision": "753e6597279ff00f98fa",
+    "url": "/event-kita/static/js/runtime-main.c4800357.js"
+  },
+  {
+    "revision": "694afcd766fe240e294159fe3f757600",
+    "url": "/event-kita/static/media/event.694afcd7.png"
+  },
+  {
+    "revision": "3fd06dc27f7ce6d7313cea0a5158cbf9",
+    "url": "/event-kita/static/media/kanan-atas.3fd06dc2.png"
+  },
+  {
+    "revision": "5361231b5d08134f4f6ef9d8ff76c10d",
+    "url": "/event-kita/static/media/kiri-bawah.5361231b.png"
+  }
+]);
